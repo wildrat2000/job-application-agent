0050fde90622ebdda1ea682b8aa7bee41665633a
Wambaa <swambaa@gmail.com>
2025-10-28 14:46:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 import pandas as pd
 from datetime import datetime
 import smtplib
-from email.mime.text import MimeText
+from email.mime.text import MiMEText
 
 # Page configuration
 st.set_page_config(
